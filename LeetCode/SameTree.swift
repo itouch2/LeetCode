d972c8bfb93046e6034ba6f4fbda1845b34698e3
Tu You <yoututouch@gmail.com>
2017-09-10 15:04:05 +0800

    125. Valid Palindrome

---
+++ LeetCode/SameTree.swift
@@ -0,0 +1,38 @@
+//
+//  SameTree.swift
+//  LeetCode
+//
+//  Created by You Tu on 2017/9/10.
+//  Copyright © 2017年 You Tu. All rights reserved.
+//
+
+
+ // Definition for a binary tree node.
+ public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class SameTree {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        guard p != nil && q != nil else {
+            if p == nil && q == nil {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        if p!.val !=  q!.val {
+            return false
+        }
+        
+        return isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right)
+    }
+}
---
+++ LeetCodeTests/SameTreeTest.swift
@@ -0,0 +1,30 @@
+//
+//  SameTreeTest.swift
+//  LeetCode
+//
+//  Created by You Tu on 2017/9/10.
+//  Copyright © 2017年 You Tu. All rights reserved.
+//
+
+import XCTest
+
+class SameTreeTest: XCTestCase {
+
+    func testIsSameTree() {
+        let sameTree = SameTree()
+        let tree1 = TreeNode(2)
+        tree1.left = TreeNode(3)
+        tree1.right = TreeNode(4)
+        
+        let tree2 = TreeNode(2)
+        tree2.left = TreeNode(3)
+        tree2.right = TreeNode(4)
+        
+//        XCTAssertTrue(sameTree.isSameTree(tree1, tree2))
+        
+        tree2.right = nil
+//        XCTAssertFalse(sameTree.isSameTree(tree1, tree2))
+        
+        XCTAssertTrue(sameTree.isSameTree(nil, nil))
+    }
+}
